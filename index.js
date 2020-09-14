792193e40c3923c2c43d621959a918bc00461fad
deboragaleano <dgaleanoservian@gmail.com>
2020-09-14 16:49:07 +0200

    phonebook-step2-GET-date and info page

--- index.js
+++ index.js
@@ -31,6 +31,17 @@ app.get('/api/persons', (req, res) => {
 })
 
 
+app.get('/info', (req, res) => {
+    const date = new Date(); 
+    const info = 
+    `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+    </div>`
+
+    res.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running in ${PORT}`);
